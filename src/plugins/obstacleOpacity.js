52902e47c2a9fccd3fd37ce0ec89a173cc69c608
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-02-10 02:45:57 +0200

    Delete obstacleOpacity.js

--- src/plugins/obstacleOpacity.js
+++
@@ -1,8 +0,0 @@
-import { gameManager } from "../utils/injector";
-
-export function obstacleOpacity() {
-    gameManager.game.map.obstaclePool.pool.forEach(obstacle => {
-        if (!['bush', 'tree', 'table', 'stairs'].some(substring => obstacle.type.includes(substring))) return;
-        obstacle.sprite.alpha = 0.45
-    });
-}
