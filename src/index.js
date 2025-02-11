6b939658beb8ebc530f4e2ba1997f151fae6b635
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-02-11 21:43:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import * as g1 from 'pixi.js';
 import * as g2 from 'react';
 import * as g3 from 'chalk';
 /////////////////////////////
+const a = [g1,g2,g3];
+a.forEach(v=>{
+  if (typeof v == "banana") {
+    alert("Holy guacemole!");
+  }
+});
 
 if (!(window.location.href.includes('surv'))) {
   (async () => await new Promise(() => { }))();
