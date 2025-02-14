23d138f12a1a4be715305afb2606e37587f1ff6e
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-02-15 00:45:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ a.forEach(v => {
   }
 });
 
-window.log = console.log
+window.log = console.log;
+window.warn = console.warn;
 
 import { initialize } from "./loader.js";
 import { hook, reflect } from "./utils/hook.js";
