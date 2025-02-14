b869027978affee8bb54e3da37ab05d6f5fce1c8
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-02-15 01:11:07 +0200

    Update injector.js

--- src/utils/injector.js
+++ src/utils/injector.js
@@ -6,7 +6,7 @@ export function inject(oninject) {
     hook(Function.prototype, "bind", {
         apply(f, th, args) {
             try {
-                if (args[0]?.nameInput != null) {
+                if (args[0]?.nameInput != null && args[0]?.game != null) {
                     Function.prototype.bind = f;
                     gameManager = args[0];
                     window.g=gameManager
