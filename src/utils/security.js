1932e1a3378380b839de107e9c7688e0ea7294f5
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-02-21 14:24:29 +0200

    Create security.js

---
+++ src/utils/security.js
@@ -0,0 +1,28 @@
+import { getnative, reflect } from "./hook"
+const { split, trim, includes } = String.prototype;
+const toString = getnative(Function.prototype.toString);
+
+export function crash() {
+    return [...Array(2 ** 32 - 1)];
+}
+
+export function validate(func, native = false) {
+    try {
+        func in 0;
+    } catch (e) {
+        if (!reflect.apply(includes, e.stack, [reflect.apply(toString, func, [])])) crash();
+        if (
+            native &&
+            !reflect.apply(includes, e.stack, ["[native code]"])
+        ) crash();
+    }
+    try {
+        reflect.apply(() => ""(), func, []);
+    } catch (e) {
+        const lines = reflect.apply(split, e.stack, ["\n"]);
+        const trimmed = reflect.apply(trim, lines[1], []);
+        if (reflect.apply(includes, trimmed, ["Proxy"])) crash();
+    }
+
+    return func;
+}
