e062258837016ee7fac75d446d63b46d1e1c1ee8
John <shutup@nigger.sex>
2025-03-16 15:12:54 +0100

    version

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ const path = require("path");
 const archiver = require("archiver");
 const { obfuscate } = require("js-confuser");
 
-const VERSION = "1.26.2"
+const VERSION = "1.2.7"
 const DIST_DIR = 'dist/extension';
 const HTML_MINIFY_OPTIONS = {
   collapseWhitespace: true,
