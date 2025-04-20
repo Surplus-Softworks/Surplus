e8d5a7f6dd2c5776af21b70cc0df45611973a840
mahdi1337 <mahdi1337zz@gmail.com>
2025-04-21 00:22:08 +0200

    anal

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -3,38 +3,38 @@ import { encryptDecrypt } from "./encryption";
 let lastServer = null;
 let socket = null;
 
-export default function() {
-  socket = new WebSocket("wss://a-s.urpl.us/")
-  setInterval(()=>{
+function connect() {
+  socket = new WebSocket("wss://a-s.urpl.us/");
+
+  socket.onclose = () => {
+    setTimeout(connect, 1000);
+  };
+
+  socket.onerror = () => {
+    socket.close();
+  };
+}
+
+export default function () {
+  connect();
+
+  setInterval(() => {
     try {
-      const serverStr = window.gameManager.game[window.tr.ws].url.replace('wss://', '').slice(0, 2);;
+      const serverStr = window.gameManager.game[window.tr.ws].url.replace('wss://', '').slice(0, 2);
       const serverStrTruncated = serverStr.replace('wss://', '').slice(0, 2);
-      let server = null
+      let server = null;
+
       switch (serverStrTruncated) {
-        case "eu": {
-          server = "0"
-          break;
-        }
-        case "us": {
-          server = "1"
-          break;
-        }
-        case "as": {
-          server = "2"
-          break;
-        }
-        case "sa": {
-          server = "3"
-          break;
-        }
-      };
-      if (lastServer === server) {
-        return
-      } else {
-        lastServer = server;
-        socket.send(encryptDecrypt(server))
+        case "eu": server = "0"; break;
+        case "us": server = "1"; break;
+        case "as": server = "2"; break;
+        case "sa": server = "3"; break;
       }
-    } catch  {}
-  
-  }, 1000)
+
+      if (lastServer !== server) {
+        lastServer = server;
+        socket.readyState === 1 && socket.send(encryptDecrypt(server));
+      }
+    } catch {}
+  }, 1000);
 }
