b9cc86893b9296ce416730c2bcfe6a00152ef853
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-07-24 17:11:21 +0300

    Update build.js

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ const path = require("path");
 const archiver = require("archiver");
 const { obfuscate } = require("js-confuser");
 
-const VERSION = "1.4.2"
+const VERSION = "1.4.3"
 const DIST_DIR = 'dist/extension';
 const HTML_MINIFY_OPTIONS = {
   collapseWhitespace: true,
