0c1cbad660c931612a9af27ba2e9e75d3176a5cb
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-09-30 21:14:01 +0300

    update shid

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ const path = require("path");
 const archiver = require("archiver");
 const { obfuscate } = require("js-confuser");
 
-const VERSION = "1.4.6"
+const VERSION = "1.5.1"
 const DIST_DIR = 'dist/extension';
 const HTML_MINIFY_OPTIONS = {
   collapseWhitespace: true,
