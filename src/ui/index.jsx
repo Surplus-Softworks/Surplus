4fe07f0a36fb2c8d020412854753a6795bb81709
mahdi1337 <mahdi1337zz@gmail.com>
2025-10-11 19:39:30 +0200

    idk

--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -8,12 +8,10 @@ import { encryptDecrypt } from '@/utils/encryption.js';
 import { globalStylesheet } from '@/ui/components/styles.css';
 import { outer, outerDocument, shadowRoot } from '@/utils/outer.js';
 
-const FONT_NAME = Array.from(
+export const FONT_NAME = Array.from(
   { length: 12 },
   () => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[Math.floor(Math.random() * 52)]
 ).join('');
-const FONT_URL =
-  'https://cdn.rawgit.com/mfd/f3d96ec7f0e8f034cc22ea73b3797b59/raw/856f1dbb8d807aabceb80b6d4f94b464df461b3e/gotham.css';
 const SETTINGS_KEY = 'c';
 const VERSION_ENDPOINT =
   'https://api.github.com/repos/Surplus-Softworks/Surplus-Releases/releases/latest';
